1ab26c3e95b596d89002af110bdba449bc5bd5ac
Jeff Brown <jeffbrowndev@gmail.com>
2019-02-28 22:56:35 -0800

    error handling PodcastCard

--- pages/components/podcastCard.js
+++ pages/components/podcastCard.js
@@ -13,12 +13,24 @@ class PodcastCard extends React.Component {
       <ScrollAnimation animateIn='fadeIn' animateOnce={true}>
         <div className={styles.podcast_card}>
           <div className={styles.podcast_date_banner}>
-            <div className={styles.podcast_date}>{this.props.date}</div>
+            <div className={styles.podcast_date}>
+              {this.props.date !== undefined
+                ? this.props.date
+                : 'No Date Provided'}
+            </div>
             <div className={styles.tail} />
           </div>
-          <h4>{ellipsis(this.props.title, 140) || 'Podcast'}</h4>
+          <h4>
+            {this.props.title !== undefined
+              ? ellipsis(this.props.title, 140)
+              : 'Podcast'}
+          </h4>
           <div href='#' className={styles.listen}>
-            <Link href={`/podcast?slug=${this.props.slug}` || '/podcasts'}>
+            <Link
+              href={`/podcast?slug=${
+                this.props.slug !== undefined ? this.props.slug : '/podcasts'
+              }`}
+            >
               <img src={'./static/images/play.svg'} />
             </Link>
             <span>LISTEN NOW</span>
