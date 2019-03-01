7383b3281aad79b39b5139512a663281ebe669c5
Jeff Brown <jeffbrowndev@gmail.com>
2019-03-01 12:23:12 -0800

    testing "next-images"

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const withSass = require('@zeit/next-sass');
 
+const withImages = require('next-images');
+
 module.exports = withSass({
   cssModules: true
 });
+
+module.exports = withImages();
 
 require('dotenv').config();
