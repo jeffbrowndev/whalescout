3c53ad3073a4fb61673f053f732abfc0908af223
Jeff <jeffbrowndev@gmail.com>
2019-03-10 16:29:58 -0700

    test

--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
 const routes = require("next-routes");
 
 // Dynamic routing for posts
-module.exports = routes().add("article", "/article/:slug");
+module.exports = routes.add("article", "/article/:slug");
