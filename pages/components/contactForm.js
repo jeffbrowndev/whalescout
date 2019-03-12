6b6f7494a053ff59659dec6ac69d129157440241
Jeff <jeffbrowndev@gmail.com>
2019-03-12 01:32:48 -0700

    installed nodemailer

--- pages/components/contactForm.js
+++ pages/components/contactForm.js
@@ -1,8 +1,25 @@
 import React from "react";
 import styles from "../scss/app.scss";
+const nodemailer = require("nodemailer");
+
+// Nodemailer config
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS
+  }
+});
 
 class ContactForm extends React.Component {
-  handleClick() {
+  sendEmail() {
+    // Create refs to gather form information and add to mailOptions below
+    const mailOptions = {
+      from: "",
+      to: "",
+      subject: "",
+      text: ""
+    };
     console.log("working");
   }
   render() {
@@ -11,7 +28,7 @@ class ContactForm extends React.Component {
         <input placeholder="Name" />
         <input placeholder="E-Mail" />
         <textarea placeholder="Your Message" />
-        <button onSubmit={this.handleClick()} className={styles.main_button}>
+        <button onSubmit={this.sendEmail()} className={styles.main_button}>
           SUBMIT
         </button>
       </form>
